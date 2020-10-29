f1ddc61052c5f049be2a5af8e7f3e4e9e2f92606
Dope-Bass <bassdanya@yandex.ru>
2020-10-29 19:54:25 +0300

    Запилил скриптос, который в будущем будет дергать класс анализатора и обрабатывать текст, и, мб, что-то еще делать. Выполнение скрипта через командную строку с одним параметром - название файла с текстом

---
+++ magic.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+import argparse
+
+import c_analyzer
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--text", default="")
+
+    return parser
+
+
+if __name__ == '__main__':
+    p = create_parser()
+    namespace = p.parse_args(sys.argv[1:])
+
+    a = c_analyzer.Analyzer(namespace.text)
+
+    print(a.reform_sentences())
+    a.close()
