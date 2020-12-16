0570f9e11f075744c48e4ee5f4e19f80561f4f70
Dope-Bass <bassdanya@yandex.ru>
2020-12-16 13:59:48 +0300

    Сделал конвертацию OpenCorpora тэгов в понятный формат

--- server/analyzer/api/views.py
+++ server/analyzer/api/views.py
@@ -2,19 +2,49 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Words, Text
 
+from django.forms.models import model_to_dict
+
+from .converter import Defines
+d = Defines()
+
 
 def make_json(query):
-    return {
-        'word': query.word,
-        'normal_form': query.normal_form,
-        'speech_part': query.speech_part,
-        'case': query.case,
-        'gender': query.gender,
-        'number': query.number,
-        'person': query.pers,
-        'voice': query.voice,
-        'role': query.role
+
+    model_dict = model_to_dict(query)
+
+    json_list = [
+        'word',
+        'normal_form',
+        'speech_part',
+        'case',
+        'gender',
+        'number',
+        'pers',
+        'voice',
+        'role'
+    ]
+    def_list = {
+        'word': None,
+        'normal_form': None,
+        'speech_part': d.speech_parts,
+        'case': d.case,
+        'gender': d.gender,
+        'number': d.number,
+        'pers': d.person,
+        'voice': d.voice,
+        'role': None
     }
+    res = {}
+    for arg in json_list:
+        if not def_list[arg]:
+            res[arg] = model_dict[arg]
+        else:
+            try:
+                res[arg] = def_list[arg][model_dict[arg]]
+            except KeyError:
+                res[arg] = model_dict[arg]
+
+    return res
 
 
 class WordViewSet(APIView):
